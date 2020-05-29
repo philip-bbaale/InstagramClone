078abe44987e502caa7f654228352d8feb22fcc6
Pip Bbaale <namugera.philip@gmail.com>
2020-05-29 12:07:11 +0300

    initial models.py

--- instagramHome/models.py
+++ instagramHome/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+#class Image(models.Model):
+    #image
+    #image_name
+    #image_caption
+    #profile_foreign_key
+    #likes
+    #comments
