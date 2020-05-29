d8fb398a7c7876f267c729790e5b5cf442c3c567
Pip Bbaale <namugera.philip@gmail.com>
2020-05-29 19:54:25 +0300

    updading home view

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,7 +21,7 @@ from instagramUsers import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='instagramUsers/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='instagramUsers/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='instagramUsers/logout.html'), name='logout'),
-    path('', include('instagramHome.urls'))
+    path('home', include('instagramHome.urls'), name='home')
 ]
--- instagramHome/urls.py
+++ instagramHome/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='instagramHome-home'),
-    path('about/', views.about, name='instagramHome-about'),
+    path('Phome', views.home, name='instagramHome-home'),
+    path('Pabout/', views.about, name='instagramHome-about'),
 ]
