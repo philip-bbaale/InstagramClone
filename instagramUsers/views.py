329bb265a193308eb22b42157b2840d65337a392
Pip Bbaale <namugera.philip@gmail.com>
2020-05-29 13:24:40 +0300

    adding forms.py

--- instagramUsers/views.py
+++ instagramUsers/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect(request, 'instagramUsers/register.html', {'form':form})
     else:
         form = UserCreationForm()
     return render(request, 'instagramUsers/register.html', {'form':form})
