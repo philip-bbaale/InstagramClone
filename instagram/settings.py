9199a212ed97818626375e7a927e8e5b0b0644e9
Pip Bbaale <namugera.philip@gmail.com>
2020-05-30 18:50:17 +0300

    adding media root and url

--- instagram/settings.py
+++ instagram/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'instagramHome-home'
 LOGIN_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
