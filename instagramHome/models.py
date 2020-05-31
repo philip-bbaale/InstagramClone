f503a35f67c47f840ff476a3bc5896734eed2134
Pip Bbaale <namugera.philip@gmail.com>
2020-05-31 20:58:24 +0300

    making post models

--- instagramHome/models.py
+++ instagramHome/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-from instagramUsers.models import Profile
+from django.contrib.auth.models import User
 
 # Create your models here
-"""
 class Post(models.Model):
     image = models.ImageField(upload_to='post_images')
     image_name = models.CharField(max_length=100)
     image_caption = models.TextField(max_length=300)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    User = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.IntegerField()
+
+    def __str__(self):
+        return self.image_name
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    """
