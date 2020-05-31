421118bd3410750d84a8430ac2caed6b8797793f
Pip Bbaale <namugera.philip@gmail.com>
2020-05-31 11:18:43 +0300

    addign post dummy data

--- instagramHome/views.py
+++ instagramHome/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'image':'xxx',
+        'image_name':"Coding",
+        'image_caption':'Love Treese Life code Eat drink SLEEP',
+        'profile':'pip_bbaale',
+        'Likes':'200',
+    }
+]
+
 def home(request):
-    return render(request, 'instagramHome/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request, 'instagramHome/home.html',context)
 
 
 def about(request):
