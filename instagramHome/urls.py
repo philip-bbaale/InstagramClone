1636b54e902e16f3c086cb39a1c8362717b31f83
Pip Bbaale <namugera.philip@gmail.com>
2020-06-02 13:44:02 +0300

    adding new post page

--- instagramHome/urls.py
+++ instagramHome/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('Phome', views.home, name='instagramHome-home'),
     path('Pabout/', views.about, name='instagramHome-about'),
+    path('Pnew_post',views.add_post, name='instagramHome-new_post')
 ]
--- instagramHome/views.py
+++ instagramHome/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Post,Comment
+from .froms import PostForm, CommentForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -14,4 +18,22 @@ def home(request):
 
 def about(request):
     return render(request, 'instagramHome/about.html', {'title': 'About'})
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            post_name = form.cleaned_data.get('image_name')
+            messages.success(request, f'Your post has been created for{post_name}!')
+            return redirect('instagramHome-home')
+    else:
+        form = PostForm()
+    
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'instagramHome/newPost.html',context, {'title':'New Post'})
 
