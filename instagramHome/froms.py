62922771db2d795b9c43736214df670fd4a759d5
Pip Bbaale <namugera.philip@gmail.com>
2020-06-02 13:09:44 +0300

    updating forms

--- instagramHome/froms.py
+++ instagramHome/froms.py
@@ -2,8 +2,9 @@ from django import forms
 
 class PostForm(forms.Form):
     image = forms.ImageField()
-    image_name = forms.CharField(label='Image Name')
-    image_caption = forms.Textarea(label='Image Caption')
+    image_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control","placeholder": "Image Name"}))
+    image_caption = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control","placeholder": "Image Caption"}))
 
 class CommentForm(forms.Form):
-    body = forms.Textarea(lable='Type a Comment')
+    body = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control","placeholder": "Leave a comment!"}))
+
