04d6eb33f063833af64d024db46c0acb755d3f0c
Pip Bbaale <namugera.philip@gmail.com>
2020-06-04 11:50:18 +0300

    adding post detail url

--- instagramHome/urls.py
+++ instagramHome/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('Phome', views.home, name='instagramHome-home'),
     path('Pabout/', views.about, name='instagramHome-about'),
-    path('Pnew_post/',views.add_post, name='instagramHome-new_post')
+    path('Pnew_post/',views.add_post, name='instagramHome-new_post'),
+    path("<int:pk>/", views.post_detail, name="post_detail"),
 ]
