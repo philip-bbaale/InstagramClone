74435b5174c84314f25a9e85486f028bdd0b8046
Pip Bbaale <namugera.philip@gmail.com>
2020-06-04 12:40:09 +0300

    adding static url

--- instagram/settings.py
+++ instagram/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'instagramHome-home'
